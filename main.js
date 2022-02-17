d308e23a5bf3284bf5e0bc9a18cf592b428b69a5
lucass0717 <lucasharbani@gmail.com>
2022-02-17 00:50:22 -0500

    fixed c3 and used eventhandler for c2

--- main.js
+++ main.js
@@ -6,19 +6,40 @@ Modified: 12/06/21
 
 */
 
+// EVENT LISTENERS
+document.getElementById("c1").addEventListener("click", circle1Clicked);
+document.getElementById("c2").addEventListener("mouseover", circle2MouseOver);
+document.getElementById("c2").addEventListener("mouseout", circle2MouseOut);
+document.getElementById("c3").addEventListener("click", circle3Clicked);
+
+
+// WHEN YOU CLICK CIRCLE 1, INCREASE COUNTER
 var counter = 0;
-    function circle1Clicked() {
-        
-        counter += 1; 
-        document.getElementById("clicks").innerHTML = counter; 
-    }
-    
-    function circle3Clicked() {
-        let curr_visibility = document.getElementById("c3").getAttribute("class"); 
-    
-        if (curr_visibility == "c3") {
-            document.getElementById("c3").setAttribute("class", "hidden"); 
-        } else {
-            document.getElementById("c3").setAttribute("class", "c3"); 
-        }
-    }
+
+function circle1Clicked() {
+    counter += 1; 
+    document.getElementById("clicks").innerHTML = counter; 
+}
+
+// WHEN YOU MOUSEOVER CIRCLE 2, SHOW BORDER
+
+function circle2MouseOver() {
+    document.getElementById("c2").setAttribute("class", "stroke");
+}
+
+function circle2MouseOut() {
+    document.getElementById("c2").setAttribute("class", "");
+}
+
+
+
+// WHEN YOU CLICK CIRCLE 3, TOGGLE VISIBILITY
+var counter3 = 0;
+var visibility = ["hidden", "stroke"];
+
+function circle3Clicked() {
+    document.getElementById("c3").setAttribute("class", visibility[counter3 % 2]); 
+    counter3++;
+}
+
+
